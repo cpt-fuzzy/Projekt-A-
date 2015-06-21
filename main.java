898cbb9f7f33f707508c9d62c3186d71f5241809
nutshellr <w.nabok@gmail.com>
2015-06-21 12:44:06 +0200

    Update main.java

--- main.java
+++ main.java
@@ -3,23 +3,20 @@ public class main {
 		
 		
 		// Generate PlayBoard 
-		FieldUnit playBoard[][] = new FieldUnit[9][9];
+		FieldUnit playBoard[][] = new FieldUnit[7][7];
 		
 		for(byte i = 0; i<playBoard.length; i++){
 			for(byte j = 0; j<playBoard[i].length;j++) {
 				
-				if((j == 0 && i == 0) ||(j == 0 && i == 1) ||(j == 0 && i == 7) ||(j == 0 && i == 8) ||
-				(j == 1 && i == 0) ||(j == 1 && i == 1) ||(j == 1 && i == 7) ||(j == 1 && i == 8) ||
-				(j == 7 && i == 0) ||(j == 7 && i == 1) ||(j == 7 && i == 7) ||(j == 7 && i == 8) ||
-				(j == 8 && i == 0) ||(j == 8 && i == 1) ||(j == 8 && i == 7) ||(j == 8 && i == 8)){
-					
+				if((j == 0 && i == 0) ||(j == 0 && i == 6) ||(j == 6 && i == 0) ||(j == 6 && i == 6))
+				{
 					FieldUnit corner = new FieldUnit(i, j, false, false, false);
 					System.out.print("   ");
 				}
 				
-				else if ((i == 3 && j == 3) || (i == 3 && j == 4) || (i == 3 && j == 5) ||
-				(i == 4 && j == 3) || (i == 4 && j == 5) ||
-				(i == 5 && j == 3) || (i == 5 && j == 4) || (i == 5 && j == 5)) {
+				else if ((i == 2 && j == 2) || (i == 2 && j == 3) || (i == 2 && j == 4) ||
+				(i == 3 && j == 2) || (i == 3 && j == 4) ||
+				(i == 4 && j == 2) || (i == 4 && j == 3) || (i == 4 && j == 4)) {
 					
 					FieldUnit hideField = new FieldUnit (i, j, true, false, true);
 					System.out.print("[x]");
