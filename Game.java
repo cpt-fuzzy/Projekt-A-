607e086b8450c9c243b86c7592d984018b4292ed
nutshellr <w.nabok@gmail.com>
2015-08-18 19:07:44 +0200

    Update Game.java
    initializeGame() ist erstmal ganz ordentlich, ich habs zu run() geändert weils kürzer ist, das Spiel kann beendet werden, sobald die im Player stehende Variable der Tokenzahl für einen der Spieler 0 erreicht

--- Game.java
+++ Game.java
@@ -1,15 +1,15 @@
-import java.util.Random();
+import java.util.Random();					
 
 public class Game {
 	
 	protected int turnCount;
-	protected Player whoseTurn;
-	private boolean gameOn = true;
-	Playboard playboard = new Playboard();
-	Player player1 = new Player("Player1", 0);
-	Player player2 = new Player("Player2", 0);
-	Random rn = new Random();
-	
+	protected Player whoseTurn;				// whoseTurn ist eigentlich unnötig, wenn man den spielablauf in der run() methode korrekt ausgeführt wird, klar das stand im UML drin, kann aber gestrichen werden
+	private boolean gameOn = true;				
+	Playboard playboard = new Playboard();			// müsste statisch sein und in run initialisiert werden
+	Player player1 = new Player("Player1", 0);		// wenn man für die Player ein Array verwendet kann man 
+	Player player2 = new Player("Player2", 0);		//hier un da ein paar Zeilen Code in der Initialisierung der Token sparen
+	Random rn = new Random();				//
+								//würde hier auch noch die statischen Objekte playBoard und Token[] deklarieren und in run() initialisieren wie 
 	/*public void determineTurnStart() {
 		int coin = rn.nextInt(2);
 		
@@ -22,11 +22,11 @@ public class Game {
 			}
 	}*/
 	
-	public void initializeGame() {
+	public static void run() {
 	playboard.generateField();
 	playboard.generateToken();
 	
-	while(gameOn == true) {
+	while(gameOn == true) {					// wenn man in der schleife eine break Bedingung einsetzt wenn einer der Spieler keine Token hat kann man sich gameOn sparen und damit Speicher 
 	player1.initializeMove();
 	player1.initializeMove();
 	player1.endTurn();
