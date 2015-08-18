a52f21da2e1f51c1f1f5144696a8c13fa23b29dc
nutshellr <w.nabok@gmail.com>
2015-08-18 18:32:38 +0200

    Update PlayBoard2.java

--- PlayBoard2.java
+++ PlayBoard2.java
@@ -1,86 +1,89 @@
+package game;
+
 public class PlayBoard {
 
-	byte base;
-	FieldUnit playBoard[][];
-	byte tokenCounters[]; // Die tokenCounters Lösung ist aus meiine Sicht etwas suboptimal, vielleicht sollte die Idee eines Counters für die Token mit affinität besser in der Player Klasse stehen...
-	Token tokens[][];
+	public byte base;
+	public FieldUnit playField[][];
+	public Token tokens[][];
 
-	public Playboard(byte base) {
+	public PlayBoard(byte base) {
 		this.base = base;
-		tokenCounters = new byte[11];
-		playBoard = new FieldUnit[base+2][base+2];
-		
+//		tokenCounters = new byte[11];
+		playField = new FieldUnit[base+2][base+2];	
 	}
+	
 	public void generateToken() {
-		for(int i = 0;i<=11;i++)
-		{
-			tokens[0][i] = new NeutralToken(((byte)0,(byte)0,1,false)); 
-		}
-		for(int i = 0;i <=7 ;i++)
-		{
-			switch(i) {
-			case 0: case 1:{
-				tokens[1][i] = 	new CircleToken((byte)0,(byte)0,3,game.player1);
-				break;
+		for(int z = 0;z<=2;z++){
+			if(z==0){
+				for(int i = 0;i<=11;i++)
+				{
+					tokens[0][i] = new NeutralToken((byte)0,(byte)0,1,false); 
+				}
 			}
-			case 2: case 3:{
-				tokens[1][i] = 	new SquareToken((byte)0,(byte)0,3,game.player1);
-				break;
-			}
-			case 4: case 5:{
-				tokens[1][i] = 	new CircleToken((byte)0,(byte)0,3,game.player1);
-				break;
-			}
-			case 6:{
-				tokens[1][i] = 	new StarToken((byte)0,(byte)0,3,player1);
-				break;
-			}
-			}
-		}
-		for(int i  = 0;i <=7 ;i++)
-		{
-			switch(i) {
-			case 0: case 1:{
-				tokens[2][i] = 	new CircleToken((byte)0,(byte)0,3,game.player2);
-				break;
-			}
-			case 2: case 3:{
-				tokens[2][i] = 	new SquareToken((byte)0,(byte)0,3,game.player2);
-				break;
-			}
-			case 4: case 5:{
-				tokens[2][i] = 	new CircleToken((byte)0,(byte)0,3,game.player2);
-				break;
-			}
-			case 6:{
-				tokens[2][i] = 	new StarToken((byte)0,(byte)0,3,game.player2);
-				break;
+			else {
+				for(int i = 0;i <=7 ;i++)
+			{
+					switch(i) {
+						case 0: case 1:{
+							tokens[z][i] = 	new CircleToken((byte)0,(byte)0,3,false, Game.player[z]);
+							break;
+						}
+						case 2: case 3:{
+							tokens[z][i] = 	new SquareToken((byte)0,(byte)0,3,false, Game.player[z]);
+							break;
+						}
+						case 4: case 5:{
+							tokens[z][i] = 	new CircleToken((byte)0,(byte)0,3,false, Game.player[z]);
+							break;
+						}
+						case 6:{
+							tokens[z][i] = 	new StarToken((byte)0,(byte)0,3,false, Game.player[z]);
+							break;
+						}
+					}
+				}
 			}
 		}
 	}
 	
 	public void generateField() {
-		for (int i = 0; i< playBoard.length; i++) 
+		for (int i = 0; i< playField.length; i++) 
 		{
-			for(int j = 0;i<playBoard[i].length; j++)
+			for(int j = 0;i<playField[i].length; j++)
 			{
 				if((i == 0 || i == base+1)&&(j == 0 || j == base+1))
 				{
-					FieldUnit notPlayableField = new FieldUnit(i,j,false);
-					playBoard[i][j] = notPlayableField;
+					FieldUnit notPlayableField = new FieldUnit(i,j, false);
+					playField[i][j] = notPlayableField;
 				}
-				else if  (((i == 2 || i == (playBoard.length - 3)) && (j >= 2 && j <= (playBoard.length - 3))) || (i == (playBoard.length - 4) && i > 1 && (j == 2 || j == (playBoard.length - 3))))
+				else if ((i <= 1 && i >= base )&&(j<= 1 && i >= base)) 
 				{
-					FieldUnit hiddenField = new FieldUnit(i,j,true);
-					playBoard[i][j] = hiddenField;
+					FieldUnit mainField = new FieldUnit(i,j, true);
+					playField[i][j] = mainField;
 				}
+//				else if  (((i == 2 || i == (playField.length - 3)) && (j >= 2 && j <= (playField.length - 3))) || (i == (playField.length - 4) && i > 1 && (j == 2 || j == (playField.length - 3))))
+//				{
+//					FieldUnit hiddenField = new FieldUnit(i,j);
+//					playField[i][j] = hiddenField;
+//				}
 				else 
 				{
-					FieldUnit normalField = new FieldUnit (i,j, false);
-					playBoard[i][j] = normalField;
+					FieldUnit borderField = new FieldUnit (i,j,false);
+					playField[i][j] = borderField;
 				}
 			}
 		}
+	}
+	
+	public boolean emptyFieldFrom(byte x,byte y,byte vectorX,byte vectorY) 
+	{
+		while(x <= base+1 && x>= 0 && y <= base+1 && y>= 0)
+		{
+			if(playField[x][y].getHasTokenOnIt()) return true;
+			x+=vectorX;
+			y+=vectorY; 
+		}
+			return false;
 	}
 
 	public byte getBase() 
@@ -93,18 +96,9 @@ public class PlayBoard {
 		this.base = base;
 	}
 
-	public byte getTokenCounter(int i) 
-	{
-		return tokenCounters[i];
-	}
-
-	public void setTokenCounter(int i, int change) 
-	{
-		tokenCounters[i] += tokenCounters[i];
-	}
 	
 	public Token getTokenOn(int x, int y)
 	{
-		return playBoard[x][y].getToken();
+		return tokens[x][y];
 	}
 }
