7d4ae8b99fd98cd365dd5541b57050d36a3c7dc3
florianvoelkers <florian.voelkers@mail.de>
2015-08-19 15:28:52 +0200

    allgemeingültige abfrage welcher move gemacht werden soll

--- Player.java
+++ Player.java
@@ -13,7 +13,32 @@ public class Player {
 		onTurn = false;
 	}
 	
-	public void initializeMove(byte vectorX, byte vectorY,byte x, byte y) {
+	public void initializeMove () {
+		System.out.print(name + " ist am Zug:\nReihe [r] oder Spalte [c] bewegen? ");
+		String choice = scanner.next();
+		if (choice.equalsIgnoreCase("r")) {
+			System.out.print("Welche Reihe? [1-5] ");
+			x = scanner.nextByte();
+			System.out.print("In welche Richtung? [-1 = links, 1 = rechts] ");
+			vectorX = scanner.nextByte();
+			vectorY = 0;
+			for (int i = 0; i < PlayBoard.base + 2; i++) {
+				Game.tokens[x][i].moveTokenTowards(vectorX, vectorY);
+			}
+		}
+		else {
+			System.out.print("Welche Spalte? [1-5] ");
+			y = scanner.nextByte();
+			System.out.print("In welche Richtung? [-1 = links, 1 = rechts] ");
+			vectorY= scanner.nextByte();
+			vectorX = 0;
+			for (int i = 0; i < PlayBoard.base + 2; i++) {
+				Game.tokens[i][y].moveTokenTowards(vectorX, vectorY);
+			}
+		}
+		
+	}
+	/*public void initializeMove(byte vectorX, byte vectorY,byte x, byte y) {
 		while(vectorX != 0 || vectorY != 0)
 		{
 			System.out.println(name + " ist am Zug: Token eingeben: ");
@@ -24,7 +49,7 @@ public class Player {
 		
 		Game.tokens[x][y].moveTokenTowards(vectorX, vectorY); // müsste noch als eine schleife umgeschrieben werden, an alle TOken die betroffen sind
 		}
-	}
+	}*/
 	
 	public void endTurn() {
 		onTurn = false;
