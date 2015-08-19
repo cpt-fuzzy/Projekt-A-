73bc864682def517e8a647794c3c00a029f1ab8d
florianvoelkers <florian.voelkers@mail.de>
2015-08-19 14:19:17 +0200

    Aufruf der random setter methode implementiert

--- PlayBoard2.java
+++ PlayBoard2.java
@@ -17,7 +17,9 @@ public class PlayBoard {
 			if(z==0){
 				for(int i = 0;i<=11;i++)
 				{
-					tokens[0][i] = new NeutralToken(1,false); 
+					tokens[0][i] = new NeutralToken(1,false);
+					tokens[0][i].setXandY(); 
+					//geht das so, dass dann für jedes Token x und y gesetzt werden?
 				}
 			}
 			else {
@@ -26,18 +28,22 @@ public class PlayBoard {
 					switch(i) {
 						case 0: case 1:{
 							tokens[z][i] = 	new CircleToken(3,false, Game.player[z]);
+							tokens[z][i].setXandY();
 							break;
 						}
 						case 2: case 3:{
 							tokens[z][i] = 	new SquareToken(3,false, Game.player[z]);
+							tokens[z][i].setXandY();
 							break;
 						}
 						case 4: case 5:{
 							tokens[z][i] = 	new TriangleToken(3,false, Game.player[z]);
+							tokens[z][i].setXandY();
 							break;
 						}
 						case 6:{
 							tokens[z][i] = 	new StarToken(3,false, Game.player[z]);
+							tokens[z][i].setXandY();
 							break;
 						}
 					}
