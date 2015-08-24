cec9764cff2c7b5a2bd228f1af00150873b8c247
nutshellr <w.nabok@gmail.com>
2015-08-24 14:32:49 +0200

    Create ShapedToken.java

---
+++ game/ShapedToken.java
@@ -0,0 +1,29 @@
+package game;
+
+public abstract class ShapedToken extends Token {
+	protected Player affinity;
+	
+	ShapedToken(int points, boolean isVisible, Player affinity){
+		super(points,isVisible);
+		this.affinity = affinity;
+	}
+	
+	protected abstract boolean moveAllowed(byte vectorX, byte vectorY);
+		
+	/*	if (playboard.emptyFieldFrom(x, y, vectorX, vectorY))
+			return true;
+		
+		else if	(vectorX == 1 && x == playField.getBase()+1||
+			 (vectorX == -1 && x == 0 )||
+			  (vectorY == 1 && y == playField.getBase()+1)||
+			   (vectorY == -1&& y == 0)) 
+			if (!(Game.playBoard.getTokenOn(x-vectorX,y-vectorY) instanceof this.getClass())||
+				this.affinity.equals(Game.playBoard.getTokenOn(x-vectorX, y-vectorY).affinity)
+			 	return false;
+
+		else return true;
+	*/
+	public Player getAffinity() {
+		return affinity;
+	}
+}
