f39e0273c0c925955cf87d605f50e07ea178a461
nutshellr <w.nabok@gmail.com>
2015-08-24 21:23:22 +0200

    Update GUI kommt

--- main.java
+++ main.java
@@ -1,14 +1,9 @@
 import game.Game;
-
 import gui.SchiebeGameGUI;
 
 public class main {
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         
+    	
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -26,8 +21,8 @@ public class main {
             java.util.logging.Logger.getLogger(SchiebeGameGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
 
-        new SchiebeGameGUI().setVisible(true);
-            */
-		Game.run();
+        SchiebeGameGUI schiebeGameGUI = new SchiebeGameGUI();
+            
+		Game.run(schiebeGameGUI);
 	}
 }
