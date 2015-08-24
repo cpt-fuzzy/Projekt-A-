fc66c94fc3f866a7f04ca6650b13c747405ffb42
nutshellr <w.nabok@gmail.com>
2015-08-24 21:45:34 +0200

    Update GUI kommt

--- game/FieldUnit.java
+++ game/FieldUnit.java
@@ -1,15 +1,18 @@
 package game;
 
+import gui.SchiebeGameGUI;
+
 public class FieldUnit {
 	
 	public byte x;
 	public byte y;
 	public boolean hasTokenOnIt;
 	
-	FieldUnit(byte x, byte y, boolean hasTokenOnIt) {
+	FieldUnit(byte x, byte y, boolean hasTokenOnIt,SchiebeGameGUI schiebeGameGUI) {
 		this.x = x;
 		this.y = y;
 		this.hasTokenOnIt = hasTokenOnIt;
+		schiebeGameGUI.setUpField(x,y);
 	}
 	
 	public byte getX () {
