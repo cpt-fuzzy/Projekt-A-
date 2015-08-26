c10bfc7eb54238f4453307e42a9984f3f6327588
nutshellr <w.nabok@gmail.com>
2015-08-26 20:42:27 +0200

    GUI Move Update

--- game/Player.java
+++ game/Player.java
@@ -15,24 +15,32 @@ public class Player {
 		onTurn = false;
 	}
 	
-	public void initializeMove () {
+	public void initializeMove (SchiebeGameGUI schiebeGameGUI) {
+		System.out.print(name + " ist am Zug:\nReihe [r] oder Spalte [c] bewegen? ");
 		String choice = scanner.next();
 		if (choice.equalsIgnoreCase("x")) {
 			byte x = scanner.nextByte();
+			System.out.print("In welche Richtung? [-1 = unten, 1 = oben] ");
 			byte vectorX = scanner.nextByte();
 			byte vectorY = (byte) 0;
-			for (int i = 0; i < Game.playBoard.getBase() + 2; i++) 
-				Game.tokens[x][i].moveTokenTowards(vectorX, vectorY);
-			
+			for (int i = 0; i < Game.playBoard.getBase() + 2; i++) {
+				if(Game.playBoard.playField[x][i].getHasTokenOnIt())
+					Game.tokens[x][i].moveTokenTowards(vectorX, vectorY, schiebeGameGUI);
+			}
 		}
 		else {
+			System.out.print("Welche Spalte? [1-5] ");
 			byte y = scanner.nextByte();
+			System.out.print("In welche Richtung? [-1 = links, 1 = rechts] ");
 			byte vectorY= scanner.nextByte();
 			byte vectorX = 0;
-			for (int i = 0; i < Game.playBoard.getBase() + 2; i++) 
-				Game.tokens[i][y].moveTokenTowards(vectorX, vectorY);
-			
+			for (int i = 0; i < Game.playBoard.getBase() + 2; i++) {
+				if(Game.playBoard.playField[i][y].getHasTokenOnIt())
+					Game.tokens[i][y].moveTokenTowards(vectorX, vectorY, schiebeGameGUI);
+			}
 		}
+		
+		schiebeGameGUI.setVisible(true);
 		
 	}
 	
