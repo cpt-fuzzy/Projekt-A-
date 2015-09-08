86e616e1cc9e34910d4d60ef26dbfe979dd1b512
nutshellr <w.nabok@gmail.com>
2015-09-08 23:53:41 +0200

    Update Player.java

--- game/Player.java
+++ game/Player.java
@@ -1,6 +1,5 @@
 package game;
 
-import java.util.Scanner;
 import gui.SchiebeGameGUI;
 
 public class Player {
@@ -8,7 +7,6 @@ public class Player {
 	private String name;
 	private int score;
 	protected boolean onTurn;
-	Scanner scanner = new Scanner(System.in);
 	
 	public Player(String name) {
 		this.name = name;
@@ -18,20 +16,27 @@ public class Player {
 	
 	public void initializeMove (char axis, byte whichAxis, byte vector, SchiebeGameGUI schiebeGameGUI)
 	{
-			byte vectorY = axis == 'y'?vector:0;
-			byte vectorX = axis == 'x'?vector:0;
-			
-				for (byte i = determineFirstToken(axis,whichAxis, vector); vector==1?i >= 0:i <= Game.playBoard.getBase()+1; i += vector==1?-1:+1) 
+		byte vectorY = axis == 'y'?vector:0;
+		byte vectorX = axis == 'x'?vector:0;
+		
+		for (byte i = determineFirstToken(axis,whichAxis, vector); vector==1?i >= 0:i <= Game.playBoard.getBase()+1; i += vector==1?-1:+1) 
+		{
+		
+			if(Game.playBoard.playField[axis=='y'?i:whichAxis][axis == 'x'?	i: whichAxis].getHasTokenOnIt())
+			{
+				try
 				{
-				
-					if(Game.playBoard.playField[axis=='y'?i:whichAxis][axis == 'x'?	i: whichAxis].getHasTokenOnIt())
-					{
-						Game.playBoard.playField[axis=='y'?i:whichAxis][axis=='x'?i:whichAxis].token.moveTokenTowards(vectorY, vectorX, schiebeGameGUI);		// Eigentliche Bewegung der Steine
-						if ((i ==0 && vector == 1)||(i == Game.playBoard.getBase()+1 && vector == -1)||(!Game.playBoard.playField[axis=='y'?i-vector:whichAxis][axis=='x'?i-vector:whichAxis].getHasTokenOnIt()))							// Falls dies der letzte zu bewegende Token ist, abbrechen 
-							break;
-					}
+					Game.playBoard.playField[axis=='y'?i:whichAxis][axis=='x'?i:whichAxis].token.moveTokenTowards(vectorY, vectorX, schiebeGameGUI);		// Eigentliche Bewegung der Steine
+					if ((i ==0 && vector == 1)||(i == Game.playBoard.getBase()+1 && vector == -1)||
+						(!Game.playBoard.playField[axis=='y'?i-vector:whichAxis][axis=='x'?i-vector:whichAxis].getHasTokenOnIt()))// Falls dies der letzte zu bewegende Token ist, abbrechen 
+						break;	
 				}
-			
+				catch ( Exception moveNotAllowed) {
+					System.out.print("Move not Allowed");
+					break;
+				}
+			}
+		}
 	}				
 
 	private byte determineFirstToken(char axis, byte whichAxis, byte vector)
