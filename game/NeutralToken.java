235da0bab5ce083ee15388d4c2414437097345fc
nutshellr <w.nabok@gmail.com>
2015-09-08 23:46:39 +0200

    Update NeutralToken.java

--- game/NeutralToken.java
+++ game/NeutralToken.java
@@ -6,20 +6,27 @@ public class NeutralToken extends Token {
 		affinity = Game.player[0];
 	}
 	
-	protected boolean moveAllowed (byte vectorX, byte vectorY) {
+	protected boolean moveAllowed (byte vectorX, byte vectorY) throws Exception {
 		if (Game.playBoard.emptyFieldFrom(x, y, vectorX, vectorY))
+		{
 			return true;
-		
+		}
 		else if	((vectorX == 1 && x == Game.playBoard.getBase()+1)||
 			 	  (vectorX == -1 && x == 0)||
 				   (vectorY == 1 && y == Game.playBoard.getBase()+1)||
 				    (vectorY == -1&& y == 0)) 
+		{
 			if(!(Game.playBoard.getTokenOn((byte)(x-vectorX),(byte)(y-vectorY)) instanceof NeutralToken))
-				return false;
-			else 
+			{
 				return true;
+			}
 			
-
+			else 
+			{
+				throw new Exception();
+			}
+			
+		}
 		else return true;
 		}
 	}
